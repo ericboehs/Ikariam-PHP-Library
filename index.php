6240b16e8eca68e7cb0b8b49f84699945d2790cb
Eric Boehs <eboehs@opubco.com>
2009-08-11 21:06:06 -0500

    Made it print_r the $ikariam array by default in index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ $ikariam = getIslandIDs();
 //echo "<pre>".
 //transportFreight($ikariam['current_city_id'],'176','46774','1000','0','0','0','0');
 
-$json = array_merge($ikariam['cities']['global']['resources'],$ikariam['cities']['46774']['resources']);
-echo json_encode(array_values($json));
+//$json = array_merge($ikariam['cities']['global']['resources'],$ikariam['cities']['46774']['resources']);
+//echo json_encode(array_values($json));
 
 //echo json_encode($ikariam);
 
-//echo "<pre>"; print_r ($ikariam); die("</pre>");
+echo "<pre>"; print_r ($ikariam); die("</pre>");
